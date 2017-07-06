6e80cbab8c95fda5133267201b63d9c6645b1fbe
GuitarmonYz <guitarmonyz@gmail.com>
2017-07-06 17:48:15 +0800

    remove binary files and lib files

--- django_back_end/auto_smart_graph/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/zhaoyan/workstuff/Auto_Smart/auto_graph/django_back_end/auto_smart_graph/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
