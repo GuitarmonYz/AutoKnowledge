a68f2bc2ae5b6ed46e6d391da9ee771a2b2a0958
GuitarmonYz <guitarmonyz@gmail.com>
2017-12-28 23:25:58 -0500

    reorgnize html blocks into components

--- autograph_front_end/src/components/querybox/querybox.vue
+++ autograph_front_end/src/components/querybox/querybox.vue
@@ -1,7 +1,30 @@
 <template>
-  <div>
-    i'm box
-  </div>
+  <div id="myVue">
+      <el-row :gutter="10">
+        <el-col v-for="(condition, idx) in conditions" :key='idx' :span="5">
+          <el-input placeholder="condition" v-model="condition.content" suffix-icon="el-icon-circle-close
+        " on-icon-click="RemoveCondition">
+            <template slot="prepend">
+              <div>{{condition.type}}</div>
+            </template>
+          </el-input>
+        </el-col>
+      </el-row>
+
+      <el-row :gutter="10">
+        <el-col v-for="(target, idx) in targets" :key='idx' :span="5">
+          <el-input placeholder="target" v-model="target.content" suffix-icon="el-icon-circle-close
+        " on-icon-click="RemoveTarget">
+            <template slot="prepend">
+              <div>{{target.type}}</div>
+            </template>
+          </el-input>
+        </el-col>
+      </el-row>
+      <el-row type="flex" justify="end">
+        <el-button @click="DrawGraph">Query</el-button>
+      </el-row>
+    </div>
 </template>
 
 <script>
--- autograph_front_end/src/components/result/result.vue
+++ autograph_front_end/src/components/result/result.vue
@@ -1,7 +1,39 @@
 <template>
-  <div>
-    I'm result   
-  </div>
+  <div id="resultVue">
+      <el-tabs type="border-card">
+        <el-tab-pane label="Graph">
+          <el-container>
+            <el-aside width="30%">
+              <el-table :data="expandable_data" style="width:100%" height="800">
+                <el-table-column type="expand">
+                  <template scope="props">
+                    <div v-for="(value, key) in props.row" :key="key">
+                      <p>{{key}}:{{value}}</p>
+                    </div>
+                  </template>
+                </el-table-column>
+                <el-table-column label="Name" :prop="prop_name">
+                </el-table-column>
+              </el-table>
+            </el-aside>
+            <el-main>
+              <div id="main" style="height:800px"></div>
+            </el-main>
+          </el-container>
+
+        </el-tab-pane>
+        <el-tab-pane label="Table">
+          <template>
+            <el-table style="width: 100%" height="800" border>
+              <el-table-column prop="type" label="Type" width="180">
+              </el-table-column>
+              <el-table-column prop="properties" label="Properties">
+              </el-table-column>
+            </el-table>
+          </template>
+        </el-tab-pane>
+      </el-tabs>
+    </div>
 </template>
 
 <script>
